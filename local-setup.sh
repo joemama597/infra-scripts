b701c89ec4d31af9e3e9225729c866ab21ac33c4
Joseph Hernandez <thecoolfoe11@gmail.com>
2023-11-22 10:16:18 -0800

    Add sudo no password to webserver and add local setup script.

--- local-setup.sh
+++ local-setup.sh
@@ -2,22 +2,6 @@
 
 # A script to install and configure nginx webserver
 
-if [ -f "/etc/sudoers.d/$USER-no-password" ]
-then
-  echo "$USER already no passsword and in sudo file"
-else
-  echo "adding $USER to sudo file with no password"
- echo "$USER ALL=(ALL:ALL) NOPASSWD: ALL" | sudo tee "/etc/sudoers.d/$USER-no-password"
-fi
-
-if (which nginx >> infra.log)
-then
-  echo "nginx already installed"
-else
-  echo "installing nginx"
-  sudo apt install -y nginx
-fi
-
 if (curl --version >> infra.log)
 then
   echo "curl already installed"
@@ -49,3 +33,6 @@ else
   echo "installing htop"
   sudo apt install -y htop
 fi
+
+# create ssh key
+# install hypervisor (multipass)
