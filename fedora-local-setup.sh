05303cac90ce53128fcb44860ea256513cf31beb
Joseph Hernandez <thecoolfoe11@gmail.com>
2023-12-06 15:16:23 -0500

    Add git configure for joe.

--- fedora-local-setup.sh
+++ fedora-local-setup.sh
@@ -16,6 +16,23 @@ else
   ssh-keygen -t ed25519 -f $HOME/.ssh/id_ed25519 -N '' <<< y
 fi
 
+if (git config --get --global user.email | grep 'thecoolfoe11@gmail.com')
+then
+  echo "git config email already set to thecoolfoe11@gmail.com"
+else
+  echo "setting git user.email to thecoolfoe11@gmail.com"
+  git config --global user.email "thecoolfoe11@gmail.com"
+fi
+
+if (git config --get --global user.name | grep 'Joseph Hernandez')
+then
+  echo "git config username already set to Joseph Hernandez"
+else
+  echo "setting username to Joseph Hernandez"
+  git config --global user.name "Joseph Hernandez"
+fi
+
+
 if (boxes -v >/dev/null)
 then
   echo "boxes already installed"
