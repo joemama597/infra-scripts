d551f3d23b74b955bed32e3ad283732b9ed00bb8
Joseph Hernandez <thecoolfoe11@gmail.com>
2023-12-18 11:05:53 -0800

    download iso to usb

--- ubuntu-image.sh
+++ ubuntu-image.sh
@@ -3,6 +3,8 @@ if (test -s ubuntu-23.10.1-desktop-amd64.iso )
 then
   echo "ubuntu already downloaded"
 else
-  echo "installing ubuntu 23.10.1 desktop"
+  echo "downloading ubuntu 23.10.1 desktop"
   curl -OLJ https://releases.ubuntu.com/23.10.1/ubuntu-23.10.1-desktop-amd64.iso
 fi
+
+sudo dd if=ubuntu-23.10.1-desktop-amd64.iso of=/dev/$(lsblk | grep -B1 'media/joe' | head -n 1 | awk '{ print $1 }') bs=1M status=progress
