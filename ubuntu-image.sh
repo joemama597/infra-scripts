9c538398e65a96a2e84381104587bef316c5837a
Joseph Hernandez <thecoolfoe11@gmail.com>
2023-12-20 09:35:15 -0800

    switches iso version from 23.10.4 to 22.04

--- ubuntu-image.sh
+++ ubuntu-image.sh
@@ -1,10 +1,10 @@
 #install ubuntu desktop 23.10.1
-if (test -s ubuntu-23.10.1-desktop-amd64.iso )
+if (test -s ubuntu-22.04-desktop-amd64.iso )
 then
   echo "ubuntu already downloaded"
 else
-  echo "downloading ubuntu 23.10.1 desktop"
-  curl -OLJ https://releases.ubuntu.com/23.10.1/ubuntu-23.10.1-desktop-amd64.iso
+  echo "downloading ubuntu 22.04 desktop"
+  curl -OLJ https://old-releases.ubuntu.com/releases/22.04/ubuntu-22.04-desktop-amd64.iso
 fi
 
-sudo dd if=ubuntu-23.10.1-desktop-amd64.iso of=/dev/$(lsblk | grep -B1 'media/joe' | head -n 1 | awk '{ print $1 }') bs=1M status=progress
+sudo dd if=ubuntu-22.04-desktop-amd64.iso of=/dev/$(lsblk | grep -B1 'media/joe' | head -n 1 | awk '{ print $1 }') bs=1M status=progress
